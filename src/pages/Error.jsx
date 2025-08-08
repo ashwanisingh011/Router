3c786f4e2faa8032d01f020f79b53a174d571c97
ashwanising011 <ashwanisingh9737@gmail.com>
2025-08-08 23:19:55 +0530

    error page edited

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,33 +1,34 @@
-import { useRouteError, Link, useNavigate } from "react-router-dom"
+import React from 'react'
+// rrd imports
+import { useRouteError, Link, useNavigate } from 'react-router-dom'
 
-// library imports
-import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
+// Library imports
+import { HomeIcon } from '@heroicons/react/24/solid'
 
 const Error = () => {
-  const error = useRouteError();
-  const navigate = useNavigate();
-
+const error = useRouteError()
+const navigate = useNavigate();
   return (
-    <div className="error">
-      <h1>Uh oh! We’ve got a problem.</h1>
+    <div className='error'>
+      <h1>Uh oh! we get an error!</h1>
       <p>{error.message || error.statusText}</p>
       <div className="flex-md">
-        <button
-          className="btn btn--dark"
-          onClick={() => navigate(-1)}
+        <button className='btn btn--dark'
+        onClick={()=>navigate(-1)} // IT will navigate back to the pervious page.
         >
-          <ArrowUturnLeftIcon width={20} />
+          <ArrowUturnLeftIcon width={20}/>
           <span>Go Back</span>
         </button>
         <Link
-          to="/"
-          className="btn btn--dark"
+          to='/'
+          className='btn btn--dark'
         >
-          <HomeIcon width={20} />
+          <HomeIcon width={20}/>
           <span>Go home</span>
         </Link>
       </div>
     </div>
   )
 }
+
 export default Error
